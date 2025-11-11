eaafb043fd94298467bd3504f089bad26819f93a
User <alunosnet@gmail.com>
2025-11-11 09:51:15 +0000

    Final da aula do dia 11/11

---
+++ M17A_Prototipo_2025_26_12T/IItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M17A_Prototipo_2025_26_12T
+{
+    internal interface IItem
+    {
+        //Validar dados
+        List<string> Validar();
+        //Adicionar
+        void Adicionar();   //TODO: base de dados
+        //Editar
+        void Editar();
+        //Apagar
+        void Apagar();
+    }
+}
---
+++ M17A_Prototipo_2025_26_12T/Livro/Livro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M17A_Prototipo_2025_26_12T.Livro
+{
+    internal class Livro : IItem
+    {
+        public void Adicionar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Apagar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
